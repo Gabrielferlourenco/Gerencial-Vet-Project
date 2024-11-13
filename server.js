f788c8fac8cc4547da48add69d9523b6b0d06db8
Gabrielferlourenco <gabriel.fl1999@gmail.com>
2024-11-13 11:08:53 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const path = require("path");
+const { Sequelize, DataTypes } = require("sequelize"); // Adicionando o Sequelize
+const app = express();
+const port = 3030;
+
+// Configuração do Sequelize
+const sequelize = new Sequelize("petshop", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+const Pet = sequelize.define(
+  "Pet",
+  {
+    petName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    breed: {
+      type: Sequelize.STRING,
+    },
+    age: {
+      type: Sequelize.INTEGER,
+    },
+    ownerName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    contact: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false, // Desativa a criação de createdAt e updatedAt
+  }
+);
+
+// Arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware para processar dados JSON no corpo da requisição
+app.use(express.json());
+
+// Rota principal para o formulário
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Rota para cadastro de pet
+app.post("/cadastro", async (req, res) => {
+  try {
+    const { petName, species, breed, age, ownerName, contact } = req.body;
+
+    // Criando um novo pet no banco de dados
+    const newPet = await Pet.create({
+      petName,
+      species,
+      breed,
+      age,
+      ownerName,
+      contact,
+    });
+
+    res
+      .status(201)
+      .json({ message: "Pet cadastrado com sucesso!", pet: newPet });
+  } catch (error) {
+    console.error("Erro ao cadastrar pet:", error);
+    res.status(500).json({ message: "Erro ao cadastrar pet." });
+  }
+});
+
+// Sincronizando o banco de dados
+sequelize
+  .sync()
+  .then(() => console.log("Banco de dados sincronizado"))
+  .catch((error) =>
+    console.error("Erro ao sincronizar o banco de dados:", error)
+  );
+
+// Iniciando o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
